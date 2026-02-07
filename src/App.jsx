b1a2bf2c959d1a57473f1e5514a92ae83fd1ef6c
lucas mello <lucasmello03@hotmail.com>
2026-02-07 00:06:19 -0300

    Fix de erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
